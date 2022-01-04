97e19350f354e37b816d05e93a32ea17c2fca193
Anne-Marie Serveto <servetonguyen@outlook.com>
2022-01-04 11:00:52 +0100

    Making the snake move correctly but without control

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
 window.onload = function () {
-    var canvas;
-    var ctxt;
-    var delay = 100;
-    var xCoord = 0;
-    var yCoord = 0;
+  var canvasWidth = 900;
+  var canvasHeight = 600;
+  var blockSize = 30;
+  var ctxt;
+  var delay = 100;
+  var xCoord = 0;
+  var yCoord = 0;
+  var snakey;
 
-    init();
+  init();
 
   function init() {
     // Creation of the canvas
-    var canvas = document.createElement("canvas");
-    canvas.width = 900;
-    canvas.height = 600;
+    var canvas = document.createElement('canvas');
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
     canvas.style.border = "1px solid";
     document.body.appendChild(canvas);
     //   How i want to draw on the html for the game
-    ctxt = canvas.getContext("2d");
+    ctxt = canvas.getContext('2d');
+    snakey = new Snake([
+      [6, 4],
+      [5, 4],
+      [4, 4],
+    ]);
     refreshCanvas();
-    
   }
-  function refreshCanvas(){
-      xCoord += 2;
-      yCoord += 2;
-    // color of the snake
-    ctxt.fillStyle = "#ff0000";
+  function refreshCanvas() {
     //Clear the canvas after set time
-    ctxt.clearRect(0,0 canvas.width, canvas.height);
-    //   30 px from Y and from X axes + size of the "snake"
-    ctxt.fillRect(xCoord, yCoord, 100, 50);
+    ctxt.clearRect(0, 0, canvasWidth, canvasHeight);
+    snakey.advance();
+    snakey.draw();
     setTimeout(refreshCanvas, delay);
+  }
+  function drawBlock(ctxt, position) {
+    var x = position[0] * blockSize;
+    var y = position[1] * blockSize;
+    ctxt.fillRect(x, y, blockSize, blockSize);
+  }
+  function Snake(body) {
+    this.body = body;
+    this.draw = function () {
+      ctxt.save();
+      // color of the snake
+      ctxt.fillStyle = "#ff0000";
+      for (var i = 0; i > this.body.length; i++) {
+        drawBlock(ctxt, this.body[i]);
+      }
+      ctxt.restore();
+    };
+    this.advance = function(){
+        var nextPosition = this.body[0].slice();
+        nextPosition[0]++ ;
+        this.body.unshift(nextPosition);
+        this.body.pop();
+    }
   }
 };
